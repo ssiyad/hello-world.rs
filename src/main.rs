c95fe464780a18d32ebe1828b08eccb7fc127f00
Sabu Siyad <hello@ssiyad.com>
2025-03-31 23:03:39 +0530

    feat: `--langs`

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,58 @@
+use std::collections::HashMap;
 use std::env;
 use std::process;
+
+struct Lang {
+    name: String,
+    code: String,
+    text: String,
+}
 
 fn main() {
     let args = env::args();
     let lang = args.skip(1).next().unwrap_or_else(|| "en".to_string());
+    let mut langs = HashMap::new();
 
-    match lang.as_str() {
-        "ar" => println!("مرحبا بالعالم!"),
-        "de" => println!("Hallo, Welt!"),
-        "en" => println!("Hello, world!"),
-        "es" => println!("¡Hola, mundo!"),
-        "fr" => println!("Bonjour, le monde!"),
-        "hi" => println!("नमस्ते, दुनिया!"),
-        "it" => println!("Ciao, mondo!"),
-        "ja" => println!("こんにちは、世界！"),
-        "ko" => println!("안녕하세요, 세계!"),
-        "ml" => println!("ഹലോ, വേൾഡ്!"),
-        "pt" => println!("Olá, mundo!"),
-        "ru" => println!("Привет, мир!"),
-        "tr" => println!("Merhaba, dünya!"),
-        "zh" => println!("你好，世界！"),
-        _ => process::exit(1),
+    for lang in [
+        ("ar", "Arabic", "مرحبا بالعالم!"),
+        ("de", "German", "Hallo, Welt!"),
+        ("en", "English", "Hello, world!"),
+        ("es", "Spanish", "¡Hola, mundo!"),
+        ("fr", "French", "Bonjour, le monde!"),
+        ("hi", "Hindi", "नमस्ते, दुनिया!"),
+        ("it", "Italian", "Ciao, mondo!"),
+        ("ja", "Japanese", "こんにちは、世界！"),
+        ("ko", "Korean", "안녕하세요, 세계!"),
+        ("ml", "Malayalam", "ഹലോ, വേൾഡ്!"),
+        ("pt", "Portuguese", "Olá, mundo!"),
+        ("ru", "Russian", "Привет, мир!"),
+        ("tr", "Turkish", "Merhaba, dünya!"),
+        ("zh", "Chinese", "你好，世界！"),
+    ] {
+        langs.insert(
+            lang.0.to_string(),
+            Lang {
+                name: lang.1.to_string(),
+                code: lang.0.to_string(),
+                text: lang.2.to_string(),
+            },
+        );
+    }
+
+    // Show list of available languages if "--langs" is passed.
+    if lang.eq("--langs") {
+        println!("Available languages:");
+        for (_, lang) in langs {
+            println!("{}: {}", lang.code, lang.name);
+        }
+        return;
+    }
+
+    match langs.get(&lang) {
+        Some(lang) => println!("{}", lang.text),
+        None => {
+            println!("Language not found. Use '--langs' to see available languages.");
+            process::exit(1);
+        }
     }
 }
